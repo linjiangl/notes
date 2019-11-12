4a62ebc154fc9b6eec04747c5f3adc0a3b2e90e0
jianglin <jl@w7.cc>
2019-11-12 10:08:50 +0800

    初始化笔记

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '学习笔记',
+  description: 'Web development notes',
+  ga: "UA-85414008-1",
+  base: "/",
+  markdown: {
+    externalLinks: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+    }
+  },
+}
